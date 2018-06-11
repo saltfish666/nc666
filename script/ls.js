d522cb46490cde509248d5789f0aeb0083f2ef7e
LiBai666 <overwall2016@gmail.com>
2018-06-11 20:53:55 +0800

    使用promis语法改写readdir

--- script/ls.js
+++ script/ls.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
+const promisify = require("util").promisify
 
 const chalk = require('chalk')
 
+const p_readdir = promisify(fs.readdir)
+const p_stat = promisify(fs.stat)
+
 let recentDir = process.env.PWD
 
-fs.readdir(recentDir,(err,files)=>{
+/*fs.readdir(recentDir,(err,files)=>{
     if(err){
       console.log(err)
     }
@@ -24,4 +28,24 @@ fs.readdir(recentDir,(err,files)=>{
             }
         })
     })
+})
+*/
+
+p_readdir(recentDir).then(files=>{
+
+    files.forEach((fileName)=>{
+
+        fs.stat(path.join(recentDir,fileName),(err,stat)=>{
+
+            if(err) throw err
+
+            if (stat.isDirectory()) {
+                console.log(chalk.blue(fileName))
+            }else{
+                console.log(fileName)
+            }
+        })
+    })
+}).catch(err=>{
+    throw err
 })
